8914cf19b7463132d2e53fb0de1fe6af88426967
Sutee Sudprasert <etipitaka@gmail.com>
2012-07-28 00:24:37 +0700

    changed admin page format for iPhone model

--- admin.py
+++ admin.py
@@ -1,4 +1,7 @@
 from django.contrib import admin
 from iphonepush.models import iPhone
 
-admin.site.register(iPhone)
+class iPhoneAdmin(admin.ModelAdmin):
+    list_display = ('udid', 'notes', 'test_phone',)
+
+admin.site.register(iPhone, iPhoneAdmin)
